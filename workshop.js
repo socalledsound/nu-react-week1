8c22fb03475ae904cc60c7b5c9813dd26af16690
chris kubick <lrs6000@gmail.com>
2021-01-23 08:39:15 -0800

    with students

--- workshop.js
+++ workshop.js
@@ -23,3 +23,13 @@ class Bootcamp {
     }
 }
 
+const myBootcamp = new Bootcamp('Creative React', 'beginner');
+const me = new Student('Christopher', 'fake@gmail.com', 'kingston vibes');
+myBootcamp.registerStudent(me);
+const asti = new Student('asti', '@fake@gmail.com', 'kingston vibes');
+myBootcamp.registerStudent(asti);
+console.log(me, asti, myBootcamp);
+
+/// now, use array methods to generate a list of all students that are in 'kingston vibes', 
+// so you can play them a nice ska track by the ethiopians: https://www.youtube.com/watch?v=L5fJQ9DYL0k
+
